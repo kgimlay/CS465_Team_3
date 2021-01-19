db0a777918ca15f16f934889be9e8305b023b90d
kgimlay <kgimlay@gmail.com>
2021-01-19 01:35:03 -0700

    Some files

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -2,13 +2,11 @@
  *
  */
 
-package EchoServerPkg;
-
 import java.net.*;
 import java.io.*;
 
 public class EchoServer {
 	public static void main( String[] args ) throws IOException {
-		
+		System.out.println("Hello!");
 	}
 }
--- src/build-run-unix.sh
+++ src/build-run-unix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 javac *.java
-javac -d 
+java EchoServer
