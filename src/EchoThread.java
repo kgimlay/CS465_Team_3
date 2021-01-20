8aeb892428f99cd3b4a9a98409e83993deb9cd2a
rduerinck <77660838+rduerinck@users.noreply.github.com>
2021-01-19 18:42:45 -0700

    Update EchoThread.java

--- src/EchoThread.java
+++ src/EchoThread.java
@@ -2,5 +2,15 @@ import java.lang.Runnable;
 import java.io.*;
 
 public class EchoThread implements Runnable {
+public void run(){ // print new connection was made 
+
+// enter loop through characters that are received 
+   // check if its an alphabet character    
+   //  it may or not update state machine accordingly
+     // echo back the characters one at a time 
 
 }
+
+
+}
+
