2f3f81c0b251a8d38262ee49ae95b01d7fda82de
yasmin vega <yasminvega98@gmail.com>
2021-01-20 16:46:07 -0700

    state machine updating code in QuitStateMachine

--- src/QuitStateMachine.java
+++ src/QuitStateMachine.java
@@ -4,19 +4,37 @@
 
 public class QuitStateMachine {
 	// class attributes
+	private int state;
 
 
 	/*
 	 * Constructor
 	 */
 	public QuitStateMachine() {
+		state = 0;
 
 	}
 
 	/*
 	 * Update state
 	 */
-	 public updateState( char character ) {
-
+	 public boolean updateState( char character ) {
+		 if(character == 'q' && state == 0){
+			 state = 1;
+		 }
+		 else if (character == 'u' && state == 1){
+			 state = 2;
+		 }
+		 else if( character == 'i' && state == 2){
+			 state = 3;
+		 }
+		 else if(character == 't' && state == 3){
+			 state = 4;
+			 return true;
+		 }
+		 else{
+			 state = 0;
+		 }
+		 return false;
 	 }
 }
