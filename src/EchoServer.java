81ed21c1622306d791e0c403e6a2276ad9c11a63
rduerinck <77660838+rduerinck@users.noreply.github.com>
2021-01-19 18:19:37 -0700

    Update EchoServer.java

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -6,7 +6,11 @@ import java.net.*;
 import java.io.*;
 
 public class EchoServer {
-	public static void main( String[] args ) throws IOException {
-		System.out.println("Hello!");
+	public static void main( String[] args ){
+        // Check for correct parameters
+        // if params correct, accept param as port number
+        
+        // loop that creates threads upon acceptance of an open          socket
+        
 	}
 }
