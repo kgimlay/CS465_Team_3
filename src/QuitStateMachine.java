9ed3bac62a5d1f822de67825e33dee3d1c1bd527
yasmin vega <yasminvega98@gmail.com>
2021-01-20 21:02:17 -0700

    Update QuitStateMachine.java
    Added detailed comments

--- src/QuitStateMachine.java
+++ src/QuitStateMachine.java
@@ -3,20 +3,27 @@
  */
 
 public class QuitStateMachine {
-	// class attributes
+	/*
+	 * State will hold one of the following values at a time: 0, 1, 2, 3, 4.
+	 * 0 is the beginning state. 4 is the final state.
+	 */
 	private int state;
 
 
 	/*
 	 * Constructor
+	 * Initialize state machine to the beginning state
 	 */
 	public QuitStateMachine() {
 		state = 0;
-
 	}
 
 	/*
 	 * Update state
+	 * General algorithm: all letters that are not 'q', 'u', 'i', 't' will result in
+	   the 0 state. If a 'q' is detected, the state will be changed to 1. If the next
+	   character is a 'u', the state will be changed to 2, and so on till the final
+	   state of 4 is reached, at which point true will be returned.
 	 */
 	 public boolean updateState( char character ) {
 		 if(character == 'q' && state == 0){
