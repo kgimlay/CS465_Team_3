621e02134e4a973ed9f599f32e5c566a3fa598eb
yasmin vega <yasminvega98@gmail.com>
2021-02-20 16:18:21 -0700

    Renamed Client class to ChatNode

--- Assignment 1/Implementation/ChatNode.java
+++ Assignment 1/Implementation/ChatNode.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 /**
  *
  */
-public class Client
+public class ChatNode
 {
 	/**
 	 *
@@ -27,7 +27,7 @@ public class Client
 	/**
 	 *
 	 */
-	Client ()
+	ChatNode ()
 	{
 
 	}
@@ -35,7 +35,7 @@ public class Client
 	/**
 	 *
 	 */
-	public void startClient()
+	public void startChatNode()
 	{
 
 	}
