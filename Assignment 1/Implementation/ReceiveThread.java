62c74c53b3fb1e8d6fece0e61e61459100fb8bfb
kgimlay <kgimlay@gmail.com>
2021-02-20 16:19:16 -0800

    Added SendThread and ReceiveThread

--- Assignment 1/Implementation/ReceiveThread.java
+++ Assignment 1/Implementation/ReceiveThread.java
@@ -1,3 +1,29 @@
+/**
+*
+*/
+
+/**
+*
+*/
 public class ReceiveThread {
-    
+   /**
+   *
+   */
+   private ServerSocket serverSocket;
+
+   /**
+   *
+   */
+   public ReceiveThread( ServerSocket serverSocket )
+   {
+      self.serverSocket = serverSocket;
+   }
+
+   /**
+   *
+   */
+   public void run()
+   {
+
+   }
 }
--- Assignment 1/Implementation/SendThread.java
+++ Assignment 1/Implementation/SendThread.java
@@ -1,3 +1,46 @@
+/**
+*
+*/
+
+import java.util.ArrayList;
+
+/**
+*
+*/
 public class SendThread {
-    
+   /**
+   *
+   */
+   private Message message;
+
+   /**
+   *
+   */
+   private ArrayList<Participant> recipients;
+
+   /**
+   *
+   */
+   public SendThread()
+   {
+      self.message = null;
+      self.recipients = null;
+   }
+
+   /**
+   *
+   */
+   public stageMessage( Message message, ArrayList<Participant> recipients )
+   {
+      self.message = message;
+      self.recipients = recipients;
+   }
+
+   /**
+   *
+   */
+   public void run()
+   {
+
+   }
 }
