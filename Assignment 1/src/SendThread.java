cfe348e29880a0b9d5bffae64b9b95675337849e
rduerinck <77660838+rduerinck@users.noreply.github.com>
2021-02-23 14:01:55 -0700

    unfinished effort on SendThread

--- Assignment 1/src/SendThread.java
+++ Assignment 1/src/SendThread.java
@@ -34,6 +34,14 @@ public class SendThread implements Runnable{
    */
    public void run()
    {
+    for( client in recipients )
+    {
+      if( client != self )//Idk how to see if it is yourself
 
+      ObjectOutputStream oos = new ObjectOutputStream(message);
+       oos.writeObject("Today");
+       objectOutputStream.writeObject(message);
+       objectOutputStream.close();
+    }
    }
 }
