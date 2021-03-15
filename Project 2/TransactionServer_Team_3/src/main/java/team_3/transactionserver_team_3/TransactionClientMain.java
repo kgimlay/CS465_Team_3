afe8ef2473e2c748b697dbb2a03b5d61b0d4e1bc
kgimlay <kgimlay@gmail.com>
2021-03-14 19:27:31 -0700

    Client and Server Proxy
    Added skeleton code to the client and server proxy classes. Also some changes to the class design/diagram is not up to date on this commit. IDK if anyone else made changes to update it. Alterations to this diagram will be made over the next day or two as we continue to design and develop the code and find things we need to change.

--- Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/TransactionClientMain.java
+++ Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/TransactionClientMain.java
@@ -1,16 +1,108 @@
 /**
-  * @authors
-  * @date 
+  * @authors Kevin Imlay
+  * @date 3/14/21
   */
 
 package team_3.transactionserver_team_3;
 
-/**
-  * 
-  */
+import java.net.ServerSocket;
+import java.net.InetAddress;
+
+/** Transaction Client is the main entrance to the client application for the
+ * system. The only transactions possible are transfers between two accounts.
+ *
+ * Configurable number of transactions to run. Randomly selects values for how
+ * much to transfer.
+ */
 public class TransactionClientMain {
+    
+    private static ServerSocket serverSocket;
+    
+    
+    /** Main entrance to client application.
+     * 
+     * Takes in configuration arguments on the command line and starts, running
+     * the number of transfers specified on randomly picked pairs of accounts.
+     * The amounts in the transfers can be bounded with optional arguments as
+     * well.
+     * 
+     * For command line arguments format, see getConfigurationFromArgs
+     */
     public static void main(String args[])
     {
+        // get configurations for command line
+        Config clientConfig = getConfigurationFromArgs(args);
         
+        // exit if the configurations are not correct
+        if (clientConfig == null)
+        {
+            System.exit(1);
+        }
+        
+        // start client, generate randoms and list of transactions/instructions
+        // to perform
+        
+        
+        // loop and perform transactions
+        
+    }
+    
+    
+    /** Gathers the command line arguments (passed in) into their appropriate
+     * configurations for the system.
+     * 
+     * @param arguments - String array of the arguments to test. Let this be
+     * the args from the main call.
+     * 
+     * @return Configuration object or Null if the configurations were not
+     * correct.
+     * 
+     * @todo Command line arguments format.
+     */
+    private static Config getConfigurationFromArgs(String arguments[])
+    {
+        // reutrn null if config incorrect
+        return null;
+    }
+    
+    
+    /** Configuration class to use with passing the configurations from the
+     * getConfigurationFromArgs method.
+     */
+    private class Config {
+        
+        // number of transactions to perform
+        int numTransactions;
+        
+        // number of accounts there are
+        // this is needed to set a max limit for random account number
+        // generation
+        int numAccounts;
+        
+        // transaction server IP and port
+        InetAddress ipPort;
+        
+        // the maximum number to transfer (optional, defaults to $100)
+        int maxTransfer = 100;
+        
+        // the minimum number to transfer (optional, defaults to $1)
+        int minTransfer = 1;
+        
+        
+        /** Construct a Config object with the configurations needed for the
+         * client.
+         * 
+         * @param numTrans
+         * @param numAccounts
+         * @param ipString
+         * @param port
+         * @param minToTransfer
+         * @param maxToTransfer
+         */
+        Config(int numTrans, int numAccounts, String ipString, int port, 
+                int minToTransfer, int maxToTransfer)
+        {
+            
+        }
     }
 }
--- Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/TransactionClientServerProxy.java
+++ Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/TransactionClientServerProxy.java
@@ -1,13 +1,101 @@
 /**
-  * @authors
-  * @date 
+  * @authors Kevin Imlay
+  * @date 3/14/21
   */
 
 package team_3.transactionserver_team_3;
 
-/**
-  * 
-  */
+import java.net.Socket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+
+/** Transaction client server proxy as a layer between the client and the
+ * transaction server.
+ * 
+ * Takes in commands from the client and sends messages to the transaction
+ * server in accordance to those commands.
+ * 
+ * @todo Need to figure out how to handle trying to access accounts that don't
+ * exist.
+ * @todo Need to figure out exactly what to do for IO exceptions.
+ */
 public class TransactionClientServerProxy {
     
+    private Socket connSocket;
+    private ObjectInputStream inObjStream;
+    private ObjectOutputStream outObjStream;
+    
+    
+    /** Create a new transaction (server proxy) to run. 
+     * 
+     * @param socket - Socket object for communication with the transaction
+     * server.
+     */
+    public TransactionClientServerProxy(Socket socket)
+    {
+        this.connSocket = socket;
+        try
+        {
+            inObjStream = new ObjectInputStream( socket.getInputStream() );
+            outObjStream = new ObjectOutputStream( socket.getOutputStream() );
+        }
+        catch (IOException ioE)
+        {
+            // handle exception
+        }
+    }
+    
+    
+    /** Opens a transaction with the transaction server.
+     * 
+     * Sends an open transaction message to the transaction server.
+     * 
+     * @throws IOException.
+     */
+    public void openTransaction()
+    {
+        
+    }
+    
+    
+    /** Close the transaction with the transaction server.
+     * 
+     * Sends a close transaction message to the transaction server.
+     * 
+     * @throws IOException.
+     */
+    public void closeTransaction()
+    {
+        
+    }
+    
+    
+    /** Write a new balance to an account in the transaction server.
+     * 
+     * Sends a write message to the transaction server with the specified
+     * account ID and new balance to write.
+     * 
+     * @throws IOException.
+     */
+    public void write(int accountID, int newBalance)
+    {
+        
+    }
+    
+    
+    /** Read the balance of an account in the transaction server.
+     * 
+     * Sends a write message to the transaction server with the account id to
+     * read the balance from. Waits for a response read message and returns the
+     * account balance.
+     * 
+     * @return int - The balance of the account.
+     * 
+     * @throws IOException.
+     */
+    public int read(int accountID)
+    {
+        return 0;   // placeholder return to let compile
+    }
 }
