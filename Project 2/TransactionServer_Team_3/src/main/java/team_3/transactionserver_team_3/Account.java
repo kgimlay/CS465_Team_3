e78bfde13db2a33c5e6ffae561015dcd2f0d57c1
yasminvn <yasminvega98@gmail.com>
2021-03-24 19:02:12 -0700

    Nothing changed, just slightly modified comments

--- Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/Account.java
+++ Project 2/TransactionServer_Team_3/src/main/java/team_3/transactionserver_team_3/Account.java
@@ -6,12 +6,12 @@
   package team_3.transactionserver_team_3;
 
   /**
-    * Account will be manipulated by Transactions. An
-      Account object holds within it a balance and account number.
+    * Account will be manipulated by Transactions and managed by AccountManager.
+    * An Account object holds within it a balance and account number.
     */
   public class Account
   {
-      // Account class specific value
+      // Accounts are assigned the value that counter currently holds
       static int counter = 0;
       
       // Account object specific values
