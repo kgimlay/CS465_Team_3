79b8ecdfa3a5e03e015651a96e584b3cafeb26a9
yasminvn <yasminvega98@gmail.com>
2021-04-18 16:14:57 -0700

    just comments

--- Project 3/ApplicationServer/src/appserver/server/LoadManager.java
+++ Project 3/ApplicationServer/src/appserver/server/LoadManager.java
@@ -9,15 +9,18 @@ import java.util.ArrayList;
  */
 public class LoadManager {
 
+    // satellites array list will hold the names of the satellites
     static ArrayList satellites = null;
+    // the last accessed index of the satellites array list
     static int lastSatelliteIndex = -1;
 
     public LoadManager() {
+        // initialize the satellites array list
         satellites = new ArrayList<String>();
     }
 
     public void satelliteAdded(String satelliteName) {
-        // add satellite
+        // add satellite's name to the array list
         satellites.add(satelliteName);
         System.out.println("[LoadManager.satelliteAdded] Added " + satelliteName);
     }
@@ -29,15 +32,20 @@ public class LoadManager {
             // implement policy that returns the satellite name according to a round robin methodology
             if( lastSatelliteIndex == satellites.size()-1 )
             {
+                // if we are at the end of the satellites array list, go back to
+                // the beginning of the array list
                 lastSatelliteIndex = 0;
             }
             else
             {
+                // if we are not at the end of the array list, interate to the
+                // next satellite
                 lastSatelliteIndex ++;
             }
         }
         
-        return (String)this.satellites.get(lastSatelliteIndex); // ... name of satellite who is supposed to take job
+        // return the name of the satellite who is supposed to take the job
+        return (String)this.satellites.get(lastSatelliteIndex);
         
     }
 }
